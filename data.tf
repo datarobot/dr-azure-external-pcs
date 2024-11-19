7b86424f00753961248b5f59f73bf6426e96a9c9
sahanasreenath <85185016+sahanasreenath@users.noreply.github.com>
2024-11-19 17:35:46 -0600

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,12 +1,4 @@
 data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "current" {}
 
-data "azurerm_key_vault_secret" "atlas_public_key" {
-  name         = "mongo-atlas-public-key"
-  key_vault_id = var.key_vault_id
-}
 
-data "azurerm_key_vault_secret" "atlas_private_key" {
-  name         = "mongo-atlas-private-key"
-  key_vault_id = var.key_vault_id
-}
