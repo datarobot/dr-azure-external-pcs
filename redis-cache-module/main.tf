cacc57bbfa07870e50fd4769e384b3bcb883f0f7
sahanasreenath <85185016+sahanasreenath@users.noreply.github.com>
2024-11-24 22:08:53 -0600

    Update main.tf

--- redis-cache-module/main.tf
+++ redis-cache-module/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_redis_cache" "redis" {
   name                = "${var.project_name}-dmaic"
-  location            = var.resource_group.location
-  resource_group_name = var.resource_group.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   capacity            = 4
   family              = "C"
   sku_name            = "Standard"
@@ -31,8 +31,8 @@ resource "azurerm_key_vault_secret" "redistoken" {
 
 resource "azurerm_private_endpoint" "redis" {
   name                = "${var.project_name}-redis"
-  location            = var.resource_group.location
-  resource_group_name = var.resource_group.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   subnet_id           = var.subnet_id
   tags                = var.tags
 
@@ -46,18 +46,18 @@ resource "azurerm_private_endpoint" "redis" {
 
 data "azurerm_private_endpoint_connection" "redis_ip" {
   name                = azurerm_private_endpoint.redis.name
-  resource_group_name = var.resource_group.name
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_private_dns_zone" "redis" {
   name                = "privatelink.redis.cache.windows.net"
-  resource_group_name = var.resource_group.name
+  resource_group_name = var.resource_group_name
   tags                = var.tags
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "redis" {
   name                  = "${var.project_name}-redis"
-  resource_group_name   = var.resource_group.name
+  resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.redis.name
   virtual_network_id    = var.network_id
   tags                  = var.tags
@@ -66,7 +66,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "redis" {
 resource "azurerm_private_dns_a_record" "redis" {
   name                = azurerm_redis_cache.redis.name
   zone_name           = azurerm_private_dns_zone.redis.name
-  resource_group_name = var.resource_group.name
+  resource_group_name = var.resource_group_name
   ttl                 = 300
   records             = [data.azurerm_private_endpoint_connection.redis_ip.private_service_connection.0.private_ip_address]
 }
